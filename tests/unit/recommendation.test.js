3fdd68889c52d02162ce91218dd20fcba3477586
Carlos Alencar <iuryalencar@poli.ufrj.br>
2021-11-30 22:40:11 -0300

    add tests upvote + refactor tests post recommendation

--- tests/unit/recommendation.test.js
+++ tests/unit/recommendation.test.js
@@ -3,28 +3,61 @@ import * as recommendationRepo from '../../src/repositories/recommendationRepo.j
 
 const sut = recommendationService;
 const mockPostRecommendationRepo = jest.spyOn(recommendationRepo, 'insert');
+const mockUpVoteRecommendationRepo = jest.spyOn(recommendationRepo, 'upVote');
 
 describe('unit RECOMMENDATION ENTITY', () => {
+    const successMessage = {
+        done: true,
+        text: expect.any(String),
+        content: {},
+    }
+
+    const errorMessage = {
+        done: false,
+        text: expect.any(String),
+        content: null
+    }
+
+    const mockRecommendation = {};
+
     beforeEach(() => {
         mockPostRecommendationRepo.mockReset();
+        mockUpVoteRecommendationRepo.mockReset();
     });
 
     describe('post recommendation', () => {
+        test('should return successMessage when object inserted', async () => {
+            mockPostRecommendationRepo.mockReturnValueOnce(mockRecommendation);
 
-        test('should return done = true when object inserted', async () => {
-            mockPostRecommendationRepo.mockReturnValueOnce({});
-
-            const { done } = await sut.post({});
+            const result = await sut.post({});
             
-            expect(done).toBeTruthy();
+            expect(result).toEqual(successMessage);
         });
 
-        test('should return done = false when object not inserted', async () => {
+        test('should return errorMessage when object not inserted', async () => {
             mockPostRecommendationRepo.mockReturnValueOnce(undefined);
 
-            const { done } = await sut.post({});
+            const result = await sut.post({});
 
-            expect(done).toBeFalsy();
+            expect(result).toEqual(errorMessage);
+        });
+    });
+
+    describe('upvote recommendation', () => {
+        test('should return successMessage when object upvoted', async () => {
+            mockPostRecommendationRepo.mockReturnValueOnce(mockRecommendation);
+
+            const result = await sut.post({});
+            
+            expect(result).toEqual(successMessage);
+        });
+
+        test('should return errorMessage when object not upvoted', async () => {
+            mockPostRecommendationRepo.mockReturnValueOnce(undefined);
+
+            const result = await sut.post({});
+
+            expect(result).toEqual(errorMessage);
         });
     });
 });
