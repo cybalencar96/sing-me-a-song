a48b13f1ff5a8849454c47278b914e11abcca9a8
Carlos Alencar <iuryalencar@poli.ufrj.br>
2021-12-01 22:31:42 -0300

    add sharedFunctions tests

---
+++ tests/unit/sharedFunctions.test.js
@@ -0,0 +1,27 @@
+import * as sharedFunctions from '../../src/utils/sharedFunctions.js';
+
+jest.spyOn(global.Math, 'floor').mockReturnValue(1)
+jest.spyOn(global.Math, 'ceil').mockReturnValue(3)
+jest.spyOn(global.Math, 'random').mockReturnValue(1)
+
+const sut = sharedFunctions;
+
+describe('shared functions', () => {
+    test('should return success message', () => {
+        const result = sut.successMessage();
+
+        expect(result).toEqual({ done: true, content: null, text: 'success' });
+    });
+
+    test('should return success message with content and modified text', () => {
+        const result = sut.successMessage({ content: {}, text: 'yay'});
+
+        expect(result).toEqual({ done: true, content: {}, text: 'yay' });
+    });
+
+    test('should return random int', () => {
+        const result = sut.getRandomInt(0,5);
+
+        expect(result).toBe(4);
+    });
+});
