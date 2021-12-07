1470947451c1993d48ed11bba0da04b3493efd90
Sidney Outeiro <sid@dcc.ufrj.br>
2021-12-07 15:53:51 -0300

    preparando para o review

--- dump.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "recommendations" (
-	"id" serial NOT NULL,
-	"name" varchar(255) NOT NULL,
-	"youtubeLink" TEXT NOT NULL,
-	"score" integer NOT NULL DEFAULT 0,
-	CONSTRAINT "recommendations_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
--- src/middlewares/serverError.js
+++
@@ -1,4 +0,0 @@
-export default function (err, req, res, next) {
-    console.log(err);
-    res.sendStatus(500);
-}
